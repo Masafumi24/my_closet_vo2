1b8e53b41b4109ad8618178efb72daab37fb1642
Masafumi24 <konchan24karats@gmail.com>
2021-03-29 20:16:51 +0900

    vue-chartjs,chart.jsの導入、及トップページへの表示

--- app/javascript/pages/topPage.vue
+++ app/javascript/pages/topPage.vue
@@ -2,13 +2,20 @@
   .topPage
     Header
     .topPageImage
+    .topPagechart
+      .topPagechartLeft
+        Chart
+      .topPagechartRight
 </template>
 
 <script>
 import Header from '../components/Header.vue'
+import Chart from '../components/Chart.vue'
+
 export default({
   components: {
-    Header
+    Header,
+    Chart
   }
 })
 </script>
