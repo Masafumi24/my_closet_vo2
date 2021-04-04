3f2788c88a4d00b0612084d819b97c17851f001c
Masafumi24 <konchan24karats@gmail.com>
2021-04-04 20:08:46 +0900

    アイテムを保存するページに遷移できるようにする

---
+++ app/javascript/app.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  router-view
+</template>
+
+<script>
+  import Vue from 'vue'
+  import VueRouter from 'vue-router'
+
+  import TopPage from './pages/topPage.vue'
+  import CreateItem from './pages/items/create.vue'
+
+  const router = new VueRouter({
+    routes: [
+      {path: '/', component: TopPage},
+      {path: '/items/create', component: CreateItem}
+    ]
+  })
+
+  Vue.use(VueRouter)
+
+  export default({ 
+    router
+  });
+</script>
