a63993613f73d3db837b70021f9d2dac0e65085b
Masafumi24 <konchan24karats@gmail.com>
2021-04-08 23:33:36 +0900

    収納画面の大まかな要素の配置

--- app/javascript/pages/items/create.vue
+++ app/javascript/pages/items/create.vue
@@ -1,10 +1,68 @@
 <template lang="pug">
-  Header
+  .itemsCreate
+    .itemsCreateTopImage
+      .itemsCreateTopImageText
+        | 収納する
+      .itemsCreateTopImageText
+        | 〜 Store in the closet 〜
+    .itemsCreateBottom
+      .itemsCreateBottomComponent
+        .itemsCreateBottomComponentTitle
+          | 画像
+        .itemsCreateBottomComponentImage
+          input(type="file").itemsCreateBottomComponentImageFile
+      .itemsCreateBottomComponent
+        .itemsCreateBottomComponentTitle
+          | 種別
+        select(v-model="selected").itemsCreateBottomComponentForm
+      .itemsCreateBottomComponent
+        .itemsCreateBottomComponentTitle
+          | 購入日
+        select(v-model="selected").itemsCreateBottomComponentForm
+      .itemsCreateBottomComponent
+        .itemsCreateBottomComponentTitle
+          | 購入場所
+        select(v-model="selected").itemsCreateBottomComponentForm
 </template>
 
 <script>
 
 </script>
 
-<style>
+<style lang="scss">
+  .itemsCreate {
+    background-color: rgba(184, 184, 184, 0.2);
+    margin: 0 auto;
+    width: 50%;
+    &TopImage {
+      box-sizing: border-box;
+      padding: 2rem;
+      // background-image: image-url("closet_new_header.jpg");
+      // background-repeat: no-repeat;
+      // background-position: top;
+      // background-size: cover;
+      height: 9rem;
+      &Text {
+        text-align: center;
+      }
+    }
+    &Bottom {
+      box-sizing: border-box;
+      padding-left: 5rem;
+      &Component {
+        &Image {
+          width: 80%;
+          display: flex;
+          justify-content: space-between;
+        }
+        &Title {
+          padding: 1rem;
+        }
+        &Form {
+          height: 2rem;
+          width: 80%;
+        }
+      }
+    }
+  }
 </style>
